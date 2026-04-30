4658b1da3042a9f4f15b78f26db27c9bce101f4d
Maksym <bienkowski.maksym@gmail.com>
2026-04-30 18:11:42 +0200

    feat: enable cuda

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import hydra
 from omegaconf import DictConfig
+import torch
 
 
 @hydra.main(version_base=None, config_name="config", config_path="conf")
 def main(_: DictConfig):
     print("hello, world")
+    print(torch.cuda.is_available())
 
 
 if __name__ == "__main__":
